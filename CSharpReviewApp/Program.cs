8bbb6932e28e13df01f21e567c1c8a7ff92c2f48
Gatcke <u24393003@tuks.co.za>
2023-05-31 15:09:23 +0200

    review part 1

---
+++ CSharpReviewApp/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("You need to learn faster");
+
+var testVar = "noob you suck";
+
+Console.WriteLine(testVar);
+
+//Basic datatypes
+
+int digits = 99;
+string anything = "abc123";
+byte block = 255;
+char single = 'a';
+
+Console.WriteLine(digits + anything + single + block);
+
+double testDouble = 0.12;
+
+//add the m,it will give you more precision
+decimal testDecimal = 0.12m;
+
+Console.WriteLine(testDecimal + " - " + testDouble);
+
+//modulelus
+int remainder = 7 % 5;
+Console.WriteLine(remainder);
+
+//remember when you devide
+double decimalRetracted = 7.0 / 2;
+double decimalNotRetracted = 7 / 2;
+Console.WriteLine(decimalNotRetracted + " - " + decimalRetracted);
+
+//+=
+
+int learnInt = 5;
+learnInt = learnInt + 1;
+learnInt += 5;
+
+Console.WriteLine(learnInt);
